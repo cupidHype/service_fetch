9d76c66f3ed6760d0d7c131a469d369e524b96ec
paintitgreen <gerwin@hyperstacksinc.com>
2020-03-19 09:56:36 +0800

    sort and paginate

--- src/app/shared/models/custom-datasource.model.ts
+++ src/app/shared/models/custom-datasource.model.ts
@@ -2,16 +2,16 @@
 import { MatTableDataSource } from '@angular/material/table';
 
 export class CustomDataSource extends MatTableDataSource<any> {
-  data = [
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', },
-    { id: '1', fn: '1', ln: '1',pos: 'Developer', }
-  ]
+  // data = [
+  //   { id: '1', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '2', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '3', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '4', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '5', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '6', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '7', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} },
+  //   { id: '8', fn: '1', ln: '1',pos: 'Developer',details: {age: 11, sex: 'Male'} }
+  // ]
 
     updateData(data) {
         this.data = [...data];
